fb14c03ffd45aeb811709f4fdf5025f2c89344e6
rudraThakar <141826284+rudraThakar@users.noreply.github.com>
2024-10-22 16:16:57 +0530

    moved hash.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,134 @@ struct WordFreq {
         this->spamFreq = s;
         this->hamFreq = h; 
     }
+};
+
+struct Node {
+    WordFreq data;      //the wordFreq class in defiened in main
+    Node* next;
+    
+    Node(WordFreq d){
+        data = d;
+        next = nullptr;
+    }
+};
+
+class HashMap{
+protected:
+    int count;
+    int size;
+
+    int hash(string s){
+      int hashVal = 0;
+      for(char c : s){
+        hashVal = 37*hashVal + c;
+      }
+      return abs(hashVal) % size;
+    }
+
+public:
+    HashMap(int sz = 997){
+        this->size = sz;
+        this->count = 0;
+};
 
 
+
+class ChainingHashMap: public HashMap{
+private:
+    vector<Node*> table;
+
+public:
+    ChainingHashMap(int s=997){
+        HashMap(s);
+        table.resize(size,nullptr);
+    }
+    ~ChainingHashMap(){
+        clear();
+    }
+
+    void insert(WordFreq data) override{
+        int index=hash(data.word);
+        Node* newNode=new Node(data);
+
+        if(!table[index]){
+            table[index]=newNode;
+            count++;
+            return;
+        }
+        Node* current = table[index];
+        while(current->next) {
+            if(current->data.word == data.word) {
+                current->data = data;
+                delete newNode;
+                return;
+            }
+            current = current->next;
+        }
+        if(current->data.word == data.word) {
+            current->data = data;
+            delete newNode;
+            return;
+        }
+        
+        current->next = newNode;
+        count++;
+    }
+}
+
+class OpenAddressingHashMap : public HashMap {
+private:
+    vector<pair<bool, WordFreq>> table;  // bool indicates if slot is occupied
+    
+public:
+    OpenAddressingHashMap(int s = 997){
+        HashMap(s);
+        table.resize(size, {false, WordFreq()});
+    }
+    
+    void insert(WordFreq data) override {
+        int index = hash(data.word);
+        int i = 0;
+        
+        while(i < size) {
+            int currentIndex = (index + i) % size;
+            
+            if(!table[currentIndex].first) {
+                table[currentIndex] = {true, data};
+                count++;
+                return;
+            }
+            else if(table[currentIndex].second.word == data.word) {
+                table[currentIndex].second = data;
+                return;
+            }
+            i++;
+        }
+        
+        cout << "Hash table is full!" << endl;
+    }
+    
+    WordFreq* search(string key) override {
+        int index = hash(key);
+        int i = 0;
+        
+        while(i < size) {
+            int currentIndex = (index + i) % size;
+            
+            if(!table[currentIndex].first) {
+                return nullptr;
+            }
+            if(table[currentIndex].second.word == key) {
+                return &(table[currentIndex].second);
+            }
+            i++;
+        }
+        return nullptr;
+    }
+    
+    void clear() override {
+        table.clear();
+        table.resize(size, {false, WordFreq()});
+        count = 0;
+    }
 };
