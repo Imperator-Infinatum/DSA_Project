a05109ea441e9a32d3fb97377cf66eedb778ab26
rudraThakar <141826284+rudraThakar@users.noreply.github.com>
2024-10-22 13:45:29 +0530

    Update and rename app.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct WordFreq {
+    string word;
+    double spamFreq;
+    double hamFreq;
+    
+    WordFreq(string w = "", double s = 0.0, double h = 0.0) {
+        this->word = w;
+        this->spamFreq = s;
+        this->hamFreq = h; 
+    }
+
+};
